8cc26b0ab788de669aaa37e52c513caf58ed5b2a
Gabriel <gabrielngoh07@gmail.com>
2025-06-18 13:01:57 +0100

    Challenge005 Finale Version

--- challenge005/app.js
+++ challenge005/app.js
@@ -1,38 +1,67 @@
 const Nav_bar = document.querySelector("#header #nav-bar");
 const menu_btn = document.querySelector("#Burger_icon");
-
 const menu_close_btn = document.querySelector(".close-img")
 const features = document.querySelector('.features>a')
 const features_lnk = document.querySelector('.features ul')
 const company = document.querySelector('.company>a')
 const arrow_iconFt = document.querySelector(".arrow__")
 const arrow_iconCo = document.querySelector(".arrow__cmp")
-const company_lmk = document.querySelector('.company ul')
+const company_lnk = document.querySelector('.company ul')
+
+/*
+*** My Own fonction !!! ***
+*/
+ const Toggle=(element, classname)=>{
+    if(element.classList.contains(classname))
+{
+      element.style.display='none'
+      element.classList.remove(classname)
+
+}
+else{
+     element.style.display='block'
+     element.classList.add(classname)
+
+}
+
+ }
+const removeElement = (element,className,arrow__)=>{
+    element.style.display="none"
+    element.classList.remove(className)
+    rotateArrowIcon(arrow__)
+}
+const rotateArrowIcon = (arrow_image)=>{
+    arrow_image.classList.toggle('rotata_attribute')
+
+}
+
+/**
+ *  Eventlistener for open and close menu buttons ,
+ *  Dropdowns for  features and company option 
+ */
 menu_btn.addEventListener('click',()=>{
-    Nav_bar.style.display="block";
+    Nav_bar.style.display="flex";
     })
+
 menu_close_btn.addEventListener('click',()=>{
     Nav_bar.style.display="none";
-    
     })
-features.addEventListener('click',()=>{
-   features_lnk.classList.toggle('display')
-   isElememt_InClass(company_lmk,'display')
-   arrow_iconFt.classList.toggle('rotata_attribute')
-    isElememt_InClass(arrow_iconCo,'rotata_attribute')
-})
-company.addEventListener('click',()=>{
-       company_lmk.classList.toggle('display')
-       isElememt_InClass(features_lnk,'display')
-       arrow_iconCo.classList.toggle('rotata_attribute')
-       isElememt_InClass(arrow_iconFt,'rotata_attribute')
 
-    
-    
+features.addEventListener('click',()=>{
+    Toggle(features_lnk,"clicked")
+    rotateArrowIcon(arrow_iconFt)
+    if(company_lnk.classList.contains('clicked'))
+     {
+        removeElement(company_lnk,"clicked",arrow_iconCo)
+     }
 })
-const isElememt_InClass=(element,classe)=>
-{
-    if(element.classList.contains(classe)){
-      element.classList.remove(classe) 
-    }
-}
+
+company.addEventListener('click',()=>{
+    Toggle(company_lnk,"clicked")
+    rotateArrowIcon(arrow_iconCo)
+    if(features_lnk.classList.contains('clicked'))
+     {
+        removeElement(features_lnk,"clicked",arrow_iconFt)
+     }
+})
+
