a67b3bc15ff12ef47538baf53ae6f447d481b85d
Gabriel <gabrielngoh07@gmail.com>
2025-06-24 20:28:45 +0100

    Another Version With JS working

--- challenge006/project/src/script.js
+++ challenge006/project/src/script.js
@@ -1,16 +1,59 @@
-const price_Monthly_Array = document.querySelectorAll("section .Price")
-const price_yearly_Array=[199.99,249.99,39.99]
+//Select Elements 
+
+const price_Monthly_Array = document.querySelectorAll("#Basic_ID header , #Pro_ID header, #Master_ID header ")
 const switch_btn =document.querySelector(".switch_btn")
 const switch_btn_cicrcle =document.querySelector(".switch_btn_Circ")
-const taille = switch_btn.clientWidth
-//Bouton switch
-switch_btn.addEventListener('click',()=>{
-    switch_btn_cicrcle.classList.toggle('slide')
-    if (switch_btn_cicrcle.classList.contains(slide)){
-      
-    }
+const proCard_Id= document.querySelector('#pro')
 
+// Previous and new Data Price: repectively Previous_Data and MAJData
+
+const Previous_Data=[
+    {Type: "Basic",prix:"19.99"},
+    {Type: "Professional",prix:"24.99"},
+    {Type: "Master",prix:"39.99"},
+    
+]
+const MAJData = [
+    {Type: "Basic",prix:"199.99"},
+    {Type: "Professional",prix:"249.99"},
+    {Type: "Master",prix:"399.99"},
+]
+
+
+// Event Listener  
+switch_btn.addEventListener('click',()=>
+{   
+    switch_btn_cicrcle.classList.toggle('slide')
+    price_Monthly_Array.forEach((element,index)=>{
+    if(switch_btn_cicrcle.classList.contains('slide'))
+     {
+        element.innerHTML=DisplayNewPrice(index)
+     }
+    else
+     {
+        element.innerHTML=DisplayPreviousPrice(index)
+     }
+                                                 })
 })
-console.log(taille)
-//Change price 
-price_Monthly_Array.forEach(Price=>Price)
+
+// Trash Code ... I was a little bit tired but it looks easy too understand
+const DisplayPreviousPrice = (index)=>{
+    return (` 
+    <header id="pro">
+      <h2>
+        <span>${Previous_Data[index].Type}</span>
+        <span class="Price">&dollar;${Previous_Data[index].prix}</span>
+      </h2>
+    </header>
+    `)
+}
+const DisplayNewPrice= (index)=>{
+    return (` 
+    <header id="pro">
+      <h2>
+        <span>${MAJData[index].Type}</span>
+        <span class="Price">&dollar;${MAJData[index].prix}</span>
+      </h2>
+    </header>
+    `)
+}
